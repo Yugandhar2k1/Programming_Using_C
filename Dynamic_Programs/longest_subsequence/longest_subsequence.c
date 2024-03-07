d093e064fb243ab6b519931dbb45fd15e280c4c7
Yugandhar <yugandhar2k1@gmail.com>
2024-03-07 12:53:43 +0530

    added output img and update code as Incresing subsequence

--- Dynamic_Programs/longest_subsequence/longest_subsequence.c
+++ Dynamic_Programs/longest_subsequence/longest_subsequence.c
@@ -5,13 +5,18 @@ void longestsubsequence(int *, int);
 void longestsubsequence(int *arr, int n)
 {
     int lis[n];
+    int prev_index[n]; // To store the previous index of each element in the LIS
     int max_length = 0;
+    int end_index = 0;
 
+    // Initialize lis and prev_index arrays
     for(int i = 0; i < n; i++)
     {
         lis[i] = 1;
+        prev_index[i] = -1; // Initialize to -1 indicating no previous index
     }
 
+    // Dynamic Programming approach to find the length of the LIS
     for(int i = 1; i < n; i++)
     {
         for(int j = 0; j < i; j++)
@@ -19,21 +24,30 @@ void longestsubsequence(int *arr, int n)
             if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
             {
                 lis[i] = lis[j] + 1;
-                
+                prev_index[i] = j; // Update the previous index
             }
         }
-        // printf(" %d  ", lis[j]);
-    }
-
-    for(int i = 0; i < n; i++)
-    {
-        if(lis[i] > max_length)
+        // Update the end index with the index having maximum LIS
+        if (lis[i] > lis[end_index])
         {
-            max_length = lis[i];
+            end_index = i;
         }
     }
 
-    printf("longest subsequence: %d", max_length);
+    // Find the maximum length of the LIS
+    max_length = lis[end_index];
+
+    // Print the length of the longest increasing subsequence
+    printf("Length of the longest increasing subsequence: %d\n", max_length);
+
+    // Print the longest increasing subsequence in ascending order
+    printf("Longest increasing subsequence: ");
+    int index = end_index;
+    while (index != -1)
+    {
+        printf("%d ", arr[index]);
+        index = prev_index[index]; // Move to the previous index
+    }
 }
 
 int main()
