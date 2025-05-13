bb5bf536e472a37f4129b820d6c4f957c16e8af3
YUGANDHAR K <114409737+Yugandhar2k1@users.noreply.github.com>
2025-05-13 11:40:22 +0530

    Delete Hacker-Earth directory

--- Hacker-Earth/sample_practice/sample.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    char name[] = "Lavanya";
-    printf("%s", name);
-}
--- Hacker-Earth/sample_practice/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-i = 0; i > len_string; i++)
-    // {
-    //     reverse[i] = str_rev[len_string-1-i];
-    // }
-    // printf("\nReverse_string: %s", reverse);
